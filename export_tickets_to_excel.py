36b1e32464351b91f641af7ff0bbc812b6fdfddd
Mark Halonen <halonen.mark@gmail.com>
2018-06-28 20:27:52 -0400

    Add csv export

--- export_tickets_to_excel.py
+++ export_tickets_to_excel.py
@@ -1,6 +1,5 @@
 import json
 import requests
-from openpyxl import Workbook
 import urllib
 import argparse
 import time
@@ -9,16 +8,20 @@ domain_name = None
 email = None
 api_key = None
 
-def fetch_ticket_ids(start, end):
+def search_tickets(start, end):
     encoded_query = urllib.urlencode({"query": "type:ticket created>%s created<%s" % (start, end)})
     url = "https://%s.zendesk.com/api/v2/search.json?%s" % (domain_name, encoded_query)
     all_results = []
     while url is not None:
         response = requests.get(url, auth=(email + "/token", api_key))
         all_results += response.json()['results']
-        print 'Fetched %d/%d ticket id\'s' % (len(all_results), response.json()['count'])
+        print "Fetched %d/%d ticket id\'s" % (len(all_results), response.json()['count'])
         url = response.json().get('next_page', None)
-    return [x["id"] for x in all_results]
+    return all_results
+
+def fetch_ticket_ids(start, end):
+    search_result = search_tickets(start, end) 
+    return [x["id"] for x in search_result]
 
 def fetch_tickets(ticket_ids):
     tickets = []
@@ -40,27 +43,65 @@ def fetch_tickets(ticket_ids):
     return tickets
 
 def fetch_comments(ticket_ids):
+    all_comments = []
     for i, ticket_id in enumerate(ticket_ids):
         url = "https://%s.zendesk.com/api/v2/tickets/%s/comments.json?include=users" % (domain_name, ticket_id)
-        all_comments = []
+        new_comments = []
         while url is not None:
             response = requests.get(url, auth=(email + "/token", api_key))
             users = response.json()["users"]
-            all_comments += response.json()["comments"]
+            new_comments += response.json()["comments"]
             url = response.json().get('next_page', None)
         
-        for comment in all_comments:
+        for comment in new_comments:
             comment["ticket_id"] = ticket_id
             comment["name"] = [x for x in users if x["id"] == comment["author_id"]][0]["name"]
-            comments.append(comment)
+            all_comments.append(comment)
         print "Fetched %d/%d ticket comments" % (i+1, len(ticket_ids))
+    return all_comments
 
-def export_to_csv(tickets, comments, file_name):
-    raise NotImplementedError("CSV not implemented")
+ticket_field_names = "id,created_at,subject,description,tags,updated_at,end_user_name,support_names".split(",")
+comment_field_names = "ticket_id,body,created_at,public,name".split(",")
+
+def export_to_csv(tickets, comments, file_name, quick=False):
+    _ext = '.csv'
+    file_name = file_name + _ext if not file_name.endswith(_ext) else file_name
+
+    print "\nExporting to CSV"
+
+    import csv
+    with open(file_name, 'wb') as csvfile:
+        writer = csv.writer(csvfile, dialect='excel')
+        if quick:
+            writer.writerow(tickets[0].keys())
+            for ticket in tickets:
+                row = []
+                for k in ticket:
+                    if k not in ticket:
+                        row.append("")
+                    elif type(ticket[k]) in (unicode, int, str):
+                        row.append(ticket[k])
+                    else:
+                        row.append(str(ticket[k]))
+                writer.writerow([v.encode('utf-8') if type(v) is unicode else v for v in row])
+        else:
+            writer.writerow(ticket_field_names)
+            for ticket in tickets:
+                row = []
+                for k in ticket_field_names:
+                    if k not in ticket:
+                        row.append("")
+                    elif type(ticket[k]) in (unicode, int, str):
+                        row.append(ticket[k])
+                    else:
+                        row.append(str(ticket[k]))
+                writer.writerow([v.encode('utf-8') if type(v) is unicode else v for v in row])
+
 
 def export_to_excel(tickets, comments, file_name):
-    ticket_field_names = "id,created_at,subject,description,tags,updated_at,end_user_name,support_names".split(",")
-    comment_field_names = "ticket_id,body,created_at,public,name".split(",")
+    from openpyxl import Workbook
+    _ext = '.xlsx'
+    file_name = file_name + _ext if not file_name.endswith(_ext) else file_name
 
     print "\nExporting to Excel file: %s" % file_name
     wb = Workbook()
@@ -100,21 +141,35 @@ def export_to_excel(tickets, comments, file_name):
     wb.save("%s" % file_name)
     print "Done"
 
+example_json_file = """
+{
+    "domain_name": "XXX",
+    "email": "XXX",
+    "api_key": "XXX",
+    "start_date": "2018-06-27",
+    "end_date": "2018-06-30",
+    "fetch_comments": false,
+    "output_file_name": "last_6_months",
+    "output_file_type": "csv"
+}
+"""
+
 def main():
     global domain_name
     global email
     global api_key
+
     parser = argparse.ArgumentParser()
     parser.add_argument("job_file", help="path to the json job file")
     args = parser.parse_args()
-    import ipdb; ipdb.set_trace()
+
     with open(args.job_file) as f:
         job = json.load(f)
 
     required_params = {"domain_name", "email", "api_key"}
     missing_params = required_params - set(job.keys())
     if missing_params:
-        raise RuntimeError("Missing required parameters: %s" % missing_params)
+        raise RuntimeError("Missing required parameters: %s. Sample json file:\n%s" % (missing_params, example_json_file))
     
     domain_name = job["domain_name"]
     email = job["email"]
@@ -122,14 +177,16 @@ def main():
 
     start_date = job.get("start_date")
     end_date = job.get("end_date")
-    excel_file_name = job.get("output_file_name", str(int(time.time())))
-    _ext = '.xlsx'
-    excel_file_name = excel_file_name + _ext if not excel_file_name.endswith(_ext) else excel_file_name
+    file_name = job.get("output_file_name", str(int(time.time())))
+    
 
-    ticket_ids = fetch_ticket_ids(start_date, end_date)
-    tickets = fetch_tickets(ticket_ids)
-    comments = fetch_comments(ticket_ids) if job.get("fetch_comments") else []
-    export_to_excel(tickets, comments, excel_file_name or str(int(time.time())))
+    # ticket_ids = fetch_ticket_ids(start_date, end_date)
+    # tickets = fetch_tickets(ticket_ids)
+    comments = [] # fetch_comments(ticket_ids) if job.get("fetch_comments") else []
+    if job.get("output_file_type", "").lower() == "csv":
+        export_to_csv(search_tickets(start_date, end_date), comments, file_name, quick=True)
+    else:
+        export_to_excel(tickets, comments, file_name)
 
 if __name__ == "__main__":
     main()
